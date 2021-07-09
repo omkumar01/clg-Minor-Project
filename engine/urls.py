86300d273c7bdf6f5aa60e7822a67880d1a5123f
omkumar01 <omkumarsahu.26@gmail.com>
2021-07-09 18:52:12 +0530

    minor changes
    added forget password view, but it does not work

--- engine/urls.py
+++ engine/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('result', ResultsPage.as_view(), name="resultspage"),
     path("LoginRegister", LoginRegister.as_view(), name="LoginRegister"),
     path("logout", LogOut.as_view(), name="logout"),
-    path("register", Register.as_view(), name="register")
+    path("register", Register.as_view(), name="register"),
+    path('ChangePassword', ChangePassword.as_view(), name='ChangePassword'),
 ]
--- engine/views.py
+++ engine/views.py
@@ -7,41 +7,66 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from .scrapbot import onPage
 
+
 class LoginRegister(View):
-
     """ Handles Login Page """
-
     def get(self, request):
 
         return render(request, 'LoginRegister.html', {})
 
     def post(self, request):
-
+        content = {}
         username = request.POST['username']
         password = request.POST['password']
         u = authenticate(request, username=username, password=password)
+        print("u = ", u)
         if u is not None:
             login(request, u)
             print("user logged in")
             return redirect("/")
         else:
-            return render(request, 'LoginRegister.html', {"login": False})
+            content = {
+                "login": False,
+                'message': "Invalid username or password",
+            }
+            print("login failed")
+            return render(request, 'LoginRegister.html', content)
 
 
 class LogOut(LoginRegister):
-
     """ Class Inherited from LoginRegister Class perform User Registration """
-
     def get(self, request):
 
         logout(request)
         return redirect('/LoginRegister')
 
 
+class ChangePassword(View):
+    def get(self, request):
+        return render(request, "changePassword.html")
+
+    def post(self, request):
+        content = {}
+        if request.POST['username']:
+            username = request.POST['username']
+            user = User.objects.get(username=username)
+            if user is None:
+                content['message'] = "User Not found"
+                return render(request, "changePassword.html", content)
+            else:
+                content['user'] = username
+                return render(request, "changePassword.html", content)
+
+        username = request.POST['name']
+        password = request.POST['password']
+        user = User.objects.get(username=username)
+        user.set_password(password)
+        user.save()
+        return HttpResponse('success')
+
+
 class Register(LoginRegister):
-
     """ Class Inherited from LoginRegister Class perform User Registration """
-
     def post(self, request):
         name = request.POST['name']
         email = request.POST['email']
@@ -49,31 +74,30 @@ class Register(LoginRegister):
         password = request.POST['password']
 
         try:
-            add_user = User.objects.create_user(
-                first_name=name, username=username, email=email, password=password, is_superuser=False)
+            add_user = User.objects.create_user(first_name=name,
+                                                username=username,
+                                                email=email,
+                                                password=password,
+                                                is_superuser=False,
+                                                is_active=False)
             add_user.save()
         except:
-            print("user registration failed")
-            return redirect("login")
+            return HttpResponse("User Registration failed")
 
-        return HttpResponse("success")
+        return HttpResponse("Verify your email before procceding")
 
 
 class LandingPage(LoginRequiredMixin, View):
-
     """A Class for just landing Page rendering"""
-
     def get(self, request):
 
         return render(request, "index.html", {})
 
 
 class ResultsPage(LoginRequiredMixin, View):
-
     """ This Class will be making a Request to Generate Data and render Result Template """
-
     def post(self, request):
-        content= {}
+        content = {}
         link = request.POST['link']
         content['data'] = onPage(link)
         content['link'] = link
